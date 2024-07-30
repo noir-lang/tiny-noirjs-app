006046b3adda4f43719fb28114f6531fb2d9fa77
josh crites <jc@joshcrites.com>
2024-07-30 16:37:56 -0400

    still failing

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { Noir } from '@noir-lang/noir_js';
-import noirjs_demo from './circuit/target/noirjs_demo.json';
+import circuit from './circuit/target/noirjs_demo.json';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const backend = new BarretenbergBackend(noirjs_demo);
-  const noir = new Noir(noirjs_demo, backend);
+  const backend = new BarretenbergBackend(circuit);
+  const noir = new Noir(circuit);
   const input = { x: 1, y: 2 };
+  const { witness } = await noir.execute(input);
   display('logs', 'Generating proof... ⌛');
-  const proof = await noir.generateFinalProof(input);
+  const proof = await backend.generateProof(witness);
   display('logs', 'Generating proof... ✅');
   display('results', proof.proof);
   display('logs', 'Verifying proof... ⌛');
-  const verification = await noir.verifyFinalProof(proof);
+  const verification = await backend.verifyProof(proof);
   if (verification) display('logs', 'Verifying proof... ✅');
 });
 
