487c25402eeeb0a1cc2705bba0cde7a17d5786d2
jimjimvalkema <jjvalkema@protonmail.com>
2024-08-08 16:21:23 +0200

    enable multi core proving

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -40,6 +40,16 @@ export default defineConfig(({ command }) => {
                     hook: 'buildStart',
                 }),
                 command === 'serve' ? wasmContentTypePlugin : [],
+                {
+                    name: "configure-response-headers",
+                    configureServer: (server) => {
+                    server.middlewares.use((_req, res, next) => {
+                        res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+                        res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+                        next();
+                    });
+                    },
+                },
             ],
         };
     }
