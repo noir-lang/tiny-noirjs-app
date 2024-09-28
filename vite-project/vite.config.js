090565b0bb76d11f2df46e834d0cb39b9280fd16
numtel <ben@latenightsketches.com>
2024-09-28 02:01:19 -0700

    chore: fix vite config to support firefox

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -44,7 +44,6 @@ export default defineConfig(({ command }) => {
                     name: "configure-response-headers",
                     configureServer: (server) => {
                     server.middlewares.use((_req, res, next) => {
-                        res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
                         res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
                         next();
                     });
