fde2ca3db24bcf784b62583b64f3d3475262098a
Josh Crites <jc@joshcrites.com>
2025-11-05 21:23:05 -0500

    simplify config

--- vite.config.js
+++ vite.config.js
@@ -3,37 +3,14 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
-    nodePolyfills({
-      include: ['buffer', 'process', 'util', 'stream'],
-      globals: {
-        Buffer: true,
-        global: true,
-        process: true,
-      },
-    }),
+    nodePolyfills(),
   ],
   optimizeDeps: {
-    esbuildOptions: {
-      target: "esnext",
-      define: {
-        global: 'globalThis',
-      },
-    },
     exclude: ["@aztec/bb.js"],
-    include: ["pino"],
   },
   resolve: {
     alias: {
       pino: "pino/browser.js",
     },
-  },
-  worker: {
-    format: "es",
-  },
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
-  },
+  }
 });
